155daa47dea538d3a8f52e8ba85714373cfec96d
crzydn <donchan.12131989@gmail.com>
2026-03-09 17:47:30 +0900

    Create next.config.js for image optimization

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  images: {
+    domains: ['example.com', 'another-example.com'], // Add your domains here
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+  },
+};
